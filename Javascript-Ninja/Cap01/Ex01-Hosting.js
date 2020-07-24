50cd9b1610284c3eb2e529e55a768d3576cbe9cb
guuhx97 <gustavorobertotb@outlook.com>
2020-07-23 22:41:36 -0300

    :recycle: Fix: atualiza as dependencias

--- Javascript-Ninja/Cap01/Ex01-Hosting.js
+++ Javascript-Ninja/Cap01/Ex01-Hosting.js
@@ -1,31 +1,28 @@
-
 //HOSTING É O IÇAMENTO DA  FUNÇÃO
 //FUNÇÕES LITERAIS SEMPRE VÃO SER IÇADAS PARA O INICIO DO CÓDIGO
 //POR ISSO É POSSIVEL CHAMA-LA ANTES DE DECLARA-LA
 
-function myFunction(){
-    var number1 = 1;
-    var number2 = 2;
+function myFunction() {
+  var number1 = 1;
+  var number2 = 2;
 
-    return sum();
+  return sum();
 
-    function sum() {  //FUNCIONA PORQUE O JS FAZ O HOSTING
-        return 1+2
-    }
+  function sum() {
+    //FUNCIONA PORQUE O JS FAZ O HOSTING
+    return 1 + 2;
+  }
 }
-console.log(myFunction())
+
+console.log(myFunction());
 
 // FUNÇÃO ANONIMAS E ARRAY FUCNTIONS NÃO FAZEM HOSTING
 //ENTÃO É NECESSÁRIO DECLARA ANTES
 
-function myFunction2(){
-    var number1 = 1;
-    var number2 = 2;
+function myFunction2() {
+  var number1 = 1;
+  var number2 = 2;
 
-    return sum;
-
-    var sum = function sum() {  //FUNCIONA PORQUE O JS FAZ O HOSTING
-        return number1+number2
-    }
+  return sum;
 }
-console.log(myFunction())
+console.log(myFunction());
